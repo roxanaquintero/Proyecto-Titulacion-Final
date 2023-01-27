014a9738f433bbb32f5ce749984e320453a40707
Roxana Quintero <rquinteron@unemi.edu.ec>
2023-01-26 21:41:15 -0500

    'configuracion-inicial'

---
+++ tareas/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def tareas(request):
+    return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')   
+
+
+
+    
